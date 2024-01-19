fe51ebd3a6fe4a6ddcc778d9e113d1f96f605cc3
dhdzmota <dhdzmota@gmail.com>
2024-01-19 02:43:11 -0600

    Included a gitignore file, a setup.py file and deleted the gitkeep and included an init py in the-code.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='the-code',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The Real Estate project.',
+    author=['Elí González', 'Daniel Hernàndez'],
+    license='',
+)
