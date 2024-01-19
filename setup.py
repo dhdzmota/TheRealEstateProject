75cd5800af5411abe0136214275f660c9cc7233a
dhdzmota <dhdzmota@gmail.com>
2024-01-19 03:05:42 -0600

    modifiy typ-o in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version='0.0.1',
     description='The Real Estate project.',
-    author=['Elí González', 'Daniel Hernàndez'],
+    author=['Elí González', 'Daniel Hernández'],
     license='',
 )
